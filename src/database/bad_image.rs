0f0b18118e10631f388b1366b26f155f14729d84
Aizawa <109516527+aiza2a@users.noreply.github.com>
2026-03-05 11:16:06 +0800

    Update bad_image.rs

--- src/database/bad_image.rs
+++ src/database/bad_image.rs
@@ -1,1 +1,18 @@
+use sqlx::sqlite::SqliteQueryResult;
+use sqlx::Result;
+use super::db::DB;
 
+pub struct BadImageEntity;
+
+impl BadImageEntity {
+    pub async fn is_bad(hash: &str) -> Result<Option<i32>> {
+        let res: Option<(i32,)> = sqlx::query_as("SELECT type FROM bad_image WHERE hash = ?")
+            .bind(hash).fetch_optional(&*DB).await?;
+        Ok(res.map(|r| r.0))
+    }
+
+    pub async fn mark(hash: &str, img_type: i32) -> Result<SqliteQueryResult> {
+        sqlx::query("INSERT OR REPLACE INTO bad_image (hash, type) VALUES (?, ?)")
+            .bind(hash).bind(img_type).execute(&*DB).await
+    }
+}
